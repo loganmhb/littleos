7fdf3dddfab1ead82b57af620a73685886fdcf45
Logan Buckley <logan.buckley@gmail.com>
2017-02-26 11:35:17 -0500

    Initialize kernel stack.

--- loader.s
+++ loader.s
@@ -1,19 +1,26 @@
 global loader
 
-    MAGIC_NUMBER equ 0x1BADB002
-    FLAGS equ 0x0
-    CHECKSUM equ -MAGIC_NUMBER
-    KERNEL_STACK_SIZE equ 4096
+MAGIC_NUMBER equ 0x1BADB002
+FLAGS equ 0x0
+CHECKSUM equ -MAGIC_NUMBER
+KERNEL_STACK_SIZE equ 4096
 
+section .bss
+align 4
 
-section .text:
+kernel_stack:
+    resb KERNEL_STACK_SIZE
+
+section .text
 align 4
     dd MAGIC_NUMBER
     dd FLAGS
     dd CHECKSUM
 
 loader:
-    mov eax, 0xCAFEBABE
+    mov esp, kernel_stack + KERNEL_STACK_SIZE ; point esp to the start of the stack,
+    mov eax, 0xCAFEBABE ; which is at the end of the memory area
+
 .loop:
     jmp .loop
     
