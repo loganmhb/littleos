e1dd4b8e8550fee5c6dc41e989c8797fdee24698
Logan Buckley <logan@hustle.com>
2019-10-13 09:09:24 -0400

    Implement if/then/else and begin/until

---
+++ kernel.f
@@ -0,0 +1,51 @@
+\ -*- Fundamental -*-
+\ Forth kernel
+
+\ CONTROL STRUCTURES
+\
+\ if syntax: `<condition> if <true clause> [else <false clause>] then`
+\ compiles to `condition branch0 <else clause loc> <true clause> branch <thenloc> | <else clause>
+: if immediate   \ compile a branch with a dummy offset, and save the location on the stack
+    ' 0branch ,  \ so we can edit it later in `else` or `then`
+    here @
+    0 , ;
+
+: then immediate
+    dup
+    here @ swap - \ calculate the offset from the address on the stack
+    swap ! ;      \ and store it
+
+: else immediate
+    ' branch ,    \ branch over the else clause to end the if section
+    here @
+    0 ,           \ dummy offset to be filled in by then
+    swap dup      \ fill in offset for first branch
+    here @
+    swap -
+    swap ! ;
+
+\ begin ... until loop
+: begin immediate
+  here @ ;
+
+: until immediate
+  ' 0branch ,
+  here @ -        \ calculate offset from value stored by `begin`
+  , ;
+
+: framebuf-size 80 25 * ;
+
+: framebuf-start 753664 ;
+
+: fb-write-cell 2 * framebuf-start + c! ;
+
+: clear-screen
+  0
+  begin
+  dup char a swap
+  fb-write-cell
+  1 +
+  dup framebuf-size >
+  until ;
+clear-screen
+end
